86b705cc37455edb00f6e5b3b1724a3d93e19b3f
Adam Britt <adamrussellbritt@gmail.com>
2023-08-25 16:00:34 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button");
+const authorSpan = document.querySelector(".author");
+const imgDiv = document.querySelector(".image-container");
+const img = document.querySelector(".img");
+
