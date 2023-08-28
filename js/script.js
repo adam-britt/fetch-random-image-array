80d02b786449855bc3542f8ce83db6b7bb2cba6e
Adam Britt <adamrussellbritt@gmail.com>
2023-08-28 10:15:06 -0500

    second commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+const getImage = async function() {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+};
